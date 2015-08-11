5306c227f81434e76beb80f9e768659ab90619f2
Rajthilak <rajthilak@megam.io>
2015-08-11 15:52:58 +0530

    websocket close bug fixed

--- tap_cluster.js
+++ tap_cluster.js
@@ -7,7 +7,6 @@ if (cluster.isMaster) {
 	// Count the machine's CPUs
 	//var cpuCount = require('os').cpus().length;
 	var cpuCount = 1;
-	console.log(cpuCount);
 	// Create a worker for each CPU
 	for (var i = 0; i < cpuCount; i += 1) {
 		cluster.fork();
